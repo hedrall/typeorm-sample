b84a89849c3340d9d98c9d894d887bce437ee240
小室　雅春 <>
2020-05-05 17:05:00 +0900

    format

--- src/entity/TestUser.ts
+++ src/entity/TestUser.ts
@@ -1,4 +1,10 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn
+} from 'typeorm';
 
 @Entity()
 export class TestUser {
@@ -6,23 +12,23 @@ export class TestUser {
   // デコレータの引数で型を定義できる
   // string型の変数に関してデフォルトではvarchar(255)型になるため、lengthをつけて宣言することもできる
   @PrimaryColumn( { type: 'varchar', length: 20 } )
-  username: string
-  
+  username: string;
+
   // emailアドレス, VARCHAR(50)型
   @PrimaryColumn( { type: 'varchar', length: 50 } )
-  email: string
-  
+  email: string;
+
   // emailアドレス, INT型
   @Column( { nullable: true } )
-  age?: number
+  age?: number;
 
   // レコードの作成時間, timestamp型
   @CreateDateColumn()
-  created_at?: string
+  created_at?: string;
 
   // レコードの更新時間, timestamp型
   @UpdateDateColumn()
-  updated_at?: string
+  updated_at?: string;
 
   // 例えば自動生成列の定義も可能, VARCHAR(10)型
   @Column( {
@@ -31,7 +37,7 @@ export class TestUser {
     generatedType: 'STORED',
     asExpression: 'date_format(created_at,\'%Y%m%d\')'
   } )
-  create_date?: string
+  create_date?: string;
 
   constructor( options: TestUser ) {
     Object.assign( this, options );
--- src/insert.ts
+++ src/insert.ts
@@ -4,7 +4,7 @@ import { TestUser } from './entity/TestUser';
 ( async () => {
   // MySQLと接続
   const connection = await createConnection( 'default' );
-  
+
   // データを挿入する
   await connection.manager.getId( TestUser, [
     {
@@ -14,9 +14,9 @@ import { TestUser } from './entity/TestUser';
     },
     {
       username: 'user2',
-      email: 'email2@example.com',
+      email: 'email2@example.com'
     }
   ] );
-  
+
   await connection.close();
 } )().catch( e => console.log( e ) );
